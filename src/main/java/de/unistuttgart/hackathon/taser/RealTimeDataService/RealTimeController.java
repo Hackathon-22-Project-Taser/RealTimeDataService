81f356cf94c14f4f39724be64af1a0f121c2378a
TheECanyon <schindler89@aol.com>
2022-05-15 14:15:47 +0200

    Add CORS Header, fix JEDIS connection

--- src/main/java/de/unistuttgart/hackathon/taser/RealTimeDataService/RealTimeController.java
+++ src/main/java/de/unistuttgart/hackathon/taser/RealTimeDataService/RealTimeController.java
@@ -3,6 +3,7 @@ package de.unistuttgart.hackathon.taser.RealTimeDataService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,10 @@ import redis.clients.jedis.JedisPooled;
 @RestController
 public class RealTimeController {
 
-    @Value("${redis.location}")
-    private String redisLocation;
-
     /**
      * Define new pool based on location in applications.properties
      */
-    JedisPooled pool = new JedisPooled(redisLocation, 6379);
+    JedisPooled pool = new JedisPooled("redis", 6379);
 
     // get Logger for this class
     private static final Logger LOGGER = LoggerFactory.getLogger(RealTimeController.class);
@@ -28,6 +26,7 @@ public class RealTimeController {
      *
      * If the specified room does not exist, this will return empty results, but doesn't fail.
      */
+    @CrossOrigin(origins = "*")
     @GetMapping("/stats/{roomIdentifier}")
     public String getStatsForRoom(@PathVariable final String roomIdentifier){
         LOGGER.info("Getting info for room " + roomIdentifier);
